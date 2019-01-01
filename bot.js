8346a5e86bdb571c5ed8d7355993750cefa9eb23
yahyaaaz <46232918+yahyaaaz@users.noreply.github.com>
2019-01-01 08:23:30 -0800

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,17 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', (326527798166552586) => {
+    client.user.setGame(`〄  Being lonely and not pathetic ♡
+ `,"http://twitch.tv/y04zgamer")
+    client.user.setStatus("dnd")
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+
+
+
+
+
+
+client.login("MzI2NTI3Nzk4MTY2NTUyNTg2.Dw0MMQ.-hkx-_dmXfIl47Dhdy6H6tELEJ0");
